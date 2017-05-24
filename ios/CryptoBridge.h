f30628a6db598d3a9188759b03702c611aafdcc5
Perry Mitchell <perry@perrymitchell.net>
2017-05-24 23:14:27 +0300

    Add bridge to iOS crypto methods

---
+++ ios/CryptoBridge.h
@@ -0,0 +1,14 @@
+//
+//  CryptoBridge.h
+//  Buttercup
+//
+//  Created by Perry Mitchell on 24/5/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface CryptoBridge : NSObject <RCTBridgeModule>
+
+@end
