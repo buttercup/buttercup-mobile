755765e99f09b55660fda98151d06fe3014c794c
Perry Mitchell <perry@perrymitchell.net>
2017-05-24 23:14:47 +0300

    Update key derivation on iOS side - double output length

--- ios/BCDerivation.m
+++ ios/BCDerivation.m
@@ -16,9 +16,9 @@ int const IV_BYTE_LEN = 16;
 + (NSString *)deriveKeyFromPassword:(NSString *)password andSalt:(NSString *)salt forRounds:(int)rounds {
     NSData *passwordData = [password dataUsingEncoding:NSUTF8StringEncoding];
     NSData *saltData = [salt dataUsingEncoding:NSUTF8StringEncoding];
-    unsigned char key[32];
-    CCKeyDerivationPBKDF(kCCPBKDF2, [passwordData bytes], [passwordData length], [saltData bytes], [saltData length], kCCPRFHmacAlgSHA256, rounds, key, 32);
-    return [BCDerivation hexStringFromData:[NSData dataWithBytes:key length:32]];
+    unsigned char key[64];
+    CCKeyDerivationPBKDF(kCCPBKDF2, [passwordData bytes], [passwordData length], [saltData bytes], [saltData length], kCCPRFHmacAlgSHA256, rounds, key, 64);
+    return [BCDerivation hexStringFromData:[NSData dataWithBytes:key length:64]];
 }
 
 + (NSString *)generateIV {
