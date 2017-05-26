ac1b77ca5a6b955660ade6b1c3ec0907eddd1dda
Perry Mitchell <perry@perrymitchell.net>
2017-05-27 00:41:10 +0300

    Cleanup test code

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from "react-native";
+import { AppRegistry } from "react-native";
 import { Provider } from "react-redux";
 import { getRouter } from "./source/routing.js";
 import { patchKeyDerivation } from "./source/library/buttercup.js";
@@ -25,41 +20,8 @@ export default class Buttercup extends Component {
                 {getRouter()}
             </Provider>
         );
-        // return (
-        //     <View style={styles.container}>
-        //         <Text style={styles.welcome}>
-        //             Welcome to React Native!
-        //         </Text>
-        //         <Text style={styles.instructions}>
-        //             To get started, edit index.ios.js
-        //         </Text>
-        //         <Text style={styles.instructions}>
-        //             Press Cmd+R to reload,{'\n'}
-        //             Cmd+D or shake for dev menu
-        //         </Text>
-        //     </View>
-        // );
     }
 
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
 
 AppRegistry.registerComponent('Buttercup', () => Buttercup);
