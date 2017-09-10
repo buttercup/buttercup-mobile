def5167204e4b12b0572dc5d4a0c0e766012e825
Perry Mitchell <perry@perrymitchell.net>
2017-09-10 16:33:53 +0300

    Rename routes reducer to nav

--- source/reducers/index.js
+++ source/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import archives from "./archives.js";
 import archiveContents from "./archiveContents.js";
-import nav from "./routes.js";
+import nav from "./nav.js";
 import addArchive from "./addArchive.js";
 import remoteExplorer from "./remoteExplorer.js";
 import entry from "./entry.js";
