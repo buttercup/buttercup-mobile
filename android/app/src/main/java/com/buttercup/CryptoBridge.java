836e64ec67e7ebfed55f044dd11719dff159caf2
Perry Mitchell <perry@perrymitchell.net>
2017-10-12 22:20:56 +0300

    Add items to android bridge

--- android/app/src/main/java/com/buttercup/CryptoBridge.java
+++ android/app/src/main/java/com/buttercup/CryptoBridge.java
@@ -12,6 +12,12 @@ public class CryptoBridge extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
+    public void decryptText(String encryptedText, String keyHex, String ivHex, String saltHex, String hmacHexKey, String hmacHex, Callback callback) {
+        String decryptedText = BCCrypto.decryptText(encryptedText, keyHex, ivHex, saltHex, hmacHexKey, hmacHex);
+        callback.invoke(null, decryptedText);
+    }
+
+    @ReactMethod
     public void deriveKeyFromPassword(String password, String salt, int rounds, Callback callback) {
         try {
             String derivedKey = BCDerivation.deriveKeyFromPassword(password, salt, rounds);
@@ -19,6 +25,24 @@ public class CryptoBridge extends ReactContextBaseJavaModule {
         } catch (Exception e) {
             callback.invoke(e.getMessage(), null);
         }
+    }
+
+    @ReactMethod
+    public void encryptText(String text, String keyHex, String saltHex, String hmacHexKey, Callback callback) {
+        String encryptedText = BCCrypto.encryptText(text, keyHex, saltHex, hmacHexKey);
+        callback.invoke(null, encryptedText);
+    }
+
+    @ReactMethod
+    public void generateSaltWithLength(int length, Callback callback) {
+        String salt = BCCrypto.generateSalt(length);
+        callback.invoke(null, salt);
+    }
+
+    @ReactMethod
+    public void generateUUIDs(Callback callback) {
+        String[] uuids = BCCrypto.generateUUIDs(50);
+        callback.invoke(null, String.join(",", uuids));
     }
 
     @Override
