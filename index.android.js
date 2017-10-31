3afe54abc3c811b1ea199c009341f41a5418d482
Perry Mitchell <perry@perrymitchell.net>
2017-10-31 21:50:35 +0200

    Setup back-button handler for android only

--- index.android.js
+++ index.android.js
@@ -1,11 +1,35 @@
 import React from "react";
-import { AppRegistry } from "react-native";
+import { AppRegistry, BackHandler } from "react-native";
 import ButtercupShared from "./index.shared.js";
+import { navigateBackIfPossible } from "./source/shared/nav.js";
+import { resetAppState } from "./source/shared/app.js";
 
 export default class Buttercup extends ButtercupShared {
     constructor(...args) {
         super(...args);
         // Android specific launch code
+        this.prepareBackButtonHandlers();
+    }
+
+    prepareBackButtonHandlers() {
+        let timer = null;
+        // Listen for the hardware back button
+        BackHandler.addEventListener("hardwareBackPress", () => {
+            if (timer === null) {
+                const willGoBack = navigateBackIfPossible();
+                if (willGoBack) {
+                    // We'll navigate back, so debounce this function
+                    timer = setTimeout(() => {
+                        timer = null;
+                    }, 400);
+                } else {
+                    // wipe our state
+                    resetAppState();
+                    return false; // not handled, let the device do its thing
+                }
+            }
+            return true; // handled
+        });
     }
 }
 
