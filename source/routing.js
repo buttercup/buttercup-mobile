f1d475ea40038b0385460235c886a0c387d6fee1
Perry Mitchell <perry@perrymitchell.net>
2018-03-11 17:37:55 +0200

    Fix react-navigation error

--- source/routing.js
+++ source/routing.js
@@ -1,16 +1,10 @@
 import React from "react";
-import {
-    Image,
-    StyleSheet,
-    Text,
-    View
-} from "react-native";
-import {
-    StackNavigator,
-    addNavigationHelpers
-} from "react-navigation";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { StackNavigator, addNavigationHelpers } from "react-navigation";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+// for react-navigation 1.0.0-beta.30
+import { createReduxBoundAddListener, createReactNavigationReduxMiddleware } from "react-navigation-redux-helpers";
 
 import ArchivesPage from "./components/ArchivesPage.js";
 import EntryPage from "./containers/EntryPage.js";
@@ -23,39 +17,45 @@ import PopupBrowser from "./containers/PopupBrowser.js";
 import GroupsPage from "./containers/GroupsPage.js";
 import LockPage from "./components/LockPage.js";
 
-export const AppNavigator = StackNavigator({
-    Home: { screen: ArchivesPage },
-    Entry: { screen: EntryPage },
-    NewEntry: { screen: NewEntryPage },
-    AddMeta: { screen: AddMetaPage },
-    AddArchive: { screen: AddArchivePage },
-    RemoteConnect: { screen: RemoteConnectPage },
-    RemoteExplorer: { screen: RemoteExplorerPage },
-    PopupBrowser: { screen: PopupBrowser },
-    GroupsPage: { screen: GroupsPage },
-    LockPage: { screen: LockPage }
-}, {
-    navigationOptions: {
-        headerTintColor: '#454545',
-        headerStyle: {
-            backgroundColor: '#ffffff',
-            borderBottomColor: '#24B5AB',
-            borderBottomWidth: 3
+export const AppNavigator = StackNavigator(
+    {
+        Home: { screen: ArchivesPage },
+        Entry: { screen: EntryPage },
+        NewEntry: { screen: NewEntryPage },
+        AddMeta: { screen: AddMetaPage },
+        AddArchive: { screen: AddArchivePage },
+        RemoteConnect: { screen: RemoteConnectPage },
+        RemoteExplorer: { screen: RemoteExplorerPage },
+        PopupBrowser: { screen: PopupBrowser },
+        GroupsPage: { screen: GroupsPage },
+        LockPage: { screen: LockPage }
+    },
+    {
+        navigationOptions: {
+            headerTintColor: "#454545",
+            headerStyle: {
+                backgroundColor: "#ffffff",
+                borderBottomColor: "#24B5AB",
+                borderBottomWidth: 3
+            }
         }
     }
-});
+);
+
+const middleware = createReactNavigationReduxMiddleware("root", state => state.nav);
+const addListener = createReduxBoundAddListener("root");
 
 const AppWithNavigationState = ({ dispatch, nav }) => (
-    <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
+    <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav, addListener })} />
 );
 
 AppWithNavigationState.propTypes = {
-    dispatch:           PropTypes.func.isRequired,
-    nav:                PropTypes.object.isRequired
+    dispatch: PropTypes.func.isRequired,
+    nav: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    nav:                state.nav
+    nav: state.nav
 });
 
 export default connect(mapStateToProps)(AppWithNavigationState);
