27906bba78460b3b39de1f56a8bc5e0e2fae2ca7
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-06-07 23:21:30 +0300

    Dynamic bits

--- ios/Crypto.m
+++ ios/Crypto.m
@@ -14,10 +14,10 @@ RCT_EXPORT_MODULE()
     return YES;
 }
 
-RCT_EXPORT_METHOD(pbkdf2:(NSString*)password:(NSString*)salt:(int)iterations:(int)bytes:(RCTPromiseResolveBlock)resolve:(RCTPromiseRejectBlock)reject) {
-    const char* utf8String = pbkdf2_derive([password cStringUsingEncoding:NSUTF8StringEncoding], [salt cStringUsingEncoding:NSUTF8StringEncoding], iterations, bytes);
+RCT_EXPORT_METHOD(pbkdf2:(NSString*)password:(NSString*)salt:(int)iterations:(int)bits:(RCTPromiseResolveBlock)resolve:(RCTPromiseRejectBlock)reject) {
+    const char* utf8String = pbkdf2_derive([password cStringUsingEncoding:NSUTF8StringEncoding], [salt cStringUsingEncoding:NSUTF8StringEncoding], iterations, bits);
     if (utf8String) {
-        resolve([BCHelpers hexStringFromData:[NSData dataWithBytes:utf8String length:64]]);
+        resolve([BCHelpers hexStringFromData:[NSData dataWithBytes:utf8String length:bits/8]]);
     } else {
         NSError *error = [NSError init];
         reject(@"pbkdf2_failed", @"Key Derivation failed.", error);
