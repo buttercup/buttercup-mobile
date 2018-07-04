bfb3907f7442c40d158441a6dab26b66d23e9a96
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-07-05 01:32:29 +0300

    Cleanup crypto file

--- source/library/crypto.js
+++ source/library/crypto.js
@@ -1,7 +1,6 @@
 import { NativeModules, Platform } from "react-native";
 import { Buffer } from "buffer";
 import { tools, vendor, Web } from "./buttercupCore.js";
-import { encode as toBase64, decode as fromBase64 } from "base-64";
 import { addToStack, getStackCount, getStackItem } from "./cache.js";
 import { Uint8Array } from "./polyfill/typedArrays.js";
 
@@ -10,10 +9,6 @@ const { Crypto } = NativeModules;
 
 const CACHE_UUID_MAX = 500;
 const CACHE_UUID_MIN = 50;
-
-export function buildCache() {
-    return Promise.all([cacheUUIDs()]);
-}
 
 function cacheUUIDs() {
     const uuidCount = getStackCount("uuid");
@@ -34,17 +29,6 @@ function cacheUUIDs() {
     return refill();
 }
 
-function constantTimeCompare(val1, val2) {
-    let sentinel;
-    if (val1.length !== val2.length) {
-        return false;
-    }
-    for (let i = 0; i <= val1.length - 1; i += 1) {
-        sentinel |= val1.charCodeAt(i) ^ val2.charCodeAt(i);
-    }
-    return sentinel === 0;
-}
-
 function internalDecrypt(encryptedComponents, keyDerivationInfo) {
     return Crypto.decryptText(
         encryptedComponents.content,
@@ -56,16 +40,16 @@ function internalDecrypt(encryptedComponents, keyDerivationInfo) {
     ).then(decryptedBase64 => new Buffer(decryptedBase64, "base64").toString("utf8"));
 }
 
-export function deriveKeyNatively(password, salt, rounds, bits) {
-    return Crypto.pbkdf2(password, salt, rounds, bits).then(derivedKeyHex =>
-        hexKeyToBuffer(derivedKeyHex)
+function deriveKeyNatively(password, salt, rounds, bits) {
+    return Crypto.pbkdf2(password, salt, rounds, bits).then(
+        derivedKeyHex => new Buffer(derivedKeyHex, "hex")
     );
 }
 
 function internalEncrypt(text, keyDerivationInfo, generatedIV) {
     const encodedText = Platform.select({
         ios: new Buffer(text, "utf8").toString("base64"),
-        android: toBase64(encodeURIComponent(text))
+        android: new Buffer(encodeURIComponent(text), "utf8").toString("base64")
     });
 
     return Crypto.encryptText(
@@ -86,44 +70,24 @@ function internalEncrypt(text, keyDerivationInfo, generatedIV) {
     });
 }
 
-export function fetchUUIDs() {
+function fetchUUIDs() {
     return Crypto.generateUUIDs(CACHE_UUID_MIN).then(res => res.split(","));
 }
 
-export function generateSalt(length) {
+function generateSalt(length) {
     return Crypto.generateSaltWithLength(length);
 }
 
-export function generateIV() {
+function generateIV() {
     return Crypto.generateIV().then(res => new Buffer(res, "hex"));
 }
 
-export function getUUID() {
+function getUUID() {
     return getStackItem("uuid");
 }
 
-export function hexKeyToBuffer(key) {
-    let hexKey = key;
-    const hexArr = [];
-    while (hexKey.length > 0) {
-        const piece = hexKey.substr(0, 2);
-        hexKey = hexKey.substr(2);
-        hexArr.push(piece);
-    }
-    const intArr = Uint8Array.from(hexArr, function(byte) {
-        return parseInt(byte, 16);
-    });
-    const buff = intArr.buffer;
-    // fix broken things
-    buff.length = intArr.length;
-    const oldToString = buff.toString;
-    buff.toString = function(mode) {
-        if (mode === "hex") {
-            return key;
-        }
-        return oldToString.call(buff, mode);
-    };
-    return buff;
+export function buildCache() {
+    return Promise.all([cacheUUIDs()]);
 }
 
 export function patchCrypto() {
