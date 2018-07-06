eb0e4a089830306cdd7186efea0ca398aea2430b
Perry Mitchell <perry@perrymitchell.net>
2018-07-06 22:20:53 +0300

    Update bash init and pipefail

--- scripts/testIOS.sh
+++ scripts/testIOS.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -8,4 +8,4 @@ SIMVER=$2
 echo Testing ${SIMNAME} with iOS ${SIMVER}...
 
 cd $THISDIR/../ios
-set -o pipefail xcodebuild -scheme Buttercup -destination "platform=iOS Simulator,name=${SIMNAME},OS=${SIMVER}" build test | xcpretty -c
+set -o pipefail && xcodebuild -scheme Buttercup -destination "platform=iOS Simulator,name=${SIMNAME},OS=${SIMVER}" build test | xcpretty -c
