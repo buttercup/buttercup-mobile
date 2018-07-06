29b490f04720983a155103127b173dc619c300e0
Perry Mitchell <perry@perrymitchell.net>
2018-07-06 22:02:48 +0300

    Add test script for IOS (for bash)

---
+++ scripts/testIOS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SIMNAME=$1
+SIMVER=$2
+
+echo Testing ${SIMNAME} with iOS ${SIMVER}...
+
+cd $THISDIR/../ios
+set -o pipefail xcodebuild -scheme Buttercup -destination "platform=iOS Simulator,name=${SIMNAME},OS=${SIMVER}" build test | xcpretty -c
