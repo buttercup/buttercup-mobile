84df8173d11e844aa2087ff7a949b283c1e4278c
Perry Mitchell <perry@perrymitchell.net>
2018-07-12 20:55:57 +0300

    Use symlinks instead of file creation for libs in tests

--- scripts/testAndroid.sh
+++ scripts/testAndroid.sh
@@ -3,8 +3,6 @@
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $THISDIR/../android
-cp app/src/main/jniLibs/x86_64-darwin/libcrypto.dylib ./app/
-cp app/src/main/jniLibs/armeabi-v7a/libcrypto.so ./app/
 ./gradlew --project-cache-dir ../.gradlecache test
 rm ./app/libcrypto.dylib
 rm ./app/libcrypto.so
