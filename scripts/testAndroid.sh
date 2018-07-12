4f3ced0fc8fb240806f23ee054f32614e97ef635
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-07-12 18:54:09 +0300

    Fix library path for tests in Android

--- scripts/testAndroid.sh
+++ scripts/testAndroid.sh
@@ -2,4 +2,9 @@
 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $THISDIR/../android && ./gradlew --project-cache-dir ../.gradlecache test
+cd $THISDIR/../android
+cp app/src/main/jniLibs/x86_64-darwin/libcrypto.dylib ./app/
+cp app/src/main/jniLibs/armeabi-v7a/libcrypto.so ./app/
+./gradlew --project-cache-dir ../.gradlecache test
+rm ./app/libcrypto.dylib
+rm ./app/libcrypto.so
