2f5af349d000db3316981b9db839d8128c9d2413
Perry Mitchell <perry@perrymitchell.net>
2018-07-14 20:34:13 +0300

    Use CryptoBridge for making android part testable

--- android/app/src/main/java/com/buttercup/Crypto.java
+++ android/app/src/main/java/com/buttercup/Crypto.java
@@ -1,132 +1,82 @@
 package com.buttercup;
 
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-
-import java.net.URLDecoder;
-import java.lang.String;
-
-public class Crypto extends ReactContextBaseJavaModule {
-    private static native String generateUUIDList(int count);
-    private static native String deriveKeyFromPassword(String password, String salt, int rounds, int bits);
-    private static native String generateSalt(int length);
-    private static native String generateRandomBytes(int length);
-    private static native String encryptCBC(
-        String encodedText,
-        String keyHex,
-        String salt,
-        String ivHex,
-        String hmacKeyHex
-    );
+public class Crypto {
     private static native String decryptCBC(
-        String encryptedText,
-        String keyHex,
-        String ivHex,
-        String salt,
-        String hmacKeyHex,
-        String hmacHex
+            String encryptedText,
+            String keyHex,
+            String ivHex,
+            String salt,
+            String hmacKeyHex,
+            String hmacHex
     );
+    private static native String deriveKeyFromPassword(String password, String salt, int rounds, int bits);
+    private static native String encryptCBC(
+            String encodedText,
+            String keyHex,
+            String salt,
+            String ivHex,
+            String hmacKeyHex
+    );
+    private static native String generateRandomBytes();
+    private static native String generateSalt(int length);
+    private static native String generateUUIDList(int count);
 
     static {
         System.loadLibrary("crypto");
     }
 
-    public Crypto(ReactApplicationContext reactContext) {
-        super(reactContext);
-    }
-
-    @ReactMethod
-    public void pbkdf2(String password, String salt, int rounds, int bits, Promise promise) {
-        String derivationHex = Crypto.deriveKeyFromPassword(password, salt, rounds, bits);
-        if (derivationHex.length() > 0) {
-            promise.resolve(derivationHex);
-            return;
-        }
-        promise.reject("Key Derivation failed.");
-    }
-
-    @ReactMethod
-    public void encryptText(
-        String encodedText,
-        String keyHex,
-        String salt,
-        String ivHex,
-        String hmacKeyHex,
-        Promise promise
+    public static String decryptText(
+            String encryptedText,
+            String keyHex,
+            String ivHex,
+            String salt,
+            String hmacKeyHex,
+            String hmacHex
     ) {
-        String encryptedText = Crypto.encryptCBC(
-            encodedText,
-            keyHex,
-            salt,
-            ivHex,
-            hmacKeyHex
+        return Crypto.decryptCBC(
+                encryptedText,
+                keyHex,
+                ivHex,
+                salt,
+                hmacKeyHex,
+                hmacHex
         );
-        if (encryptedText == null) {
-            promise.reject("Encryption failed.");
-            return;
-        }
-        promise.resolve(encryptedText);
     }
 
-    @ReactMethod
-    public void decryptText(
-        String encryptedText,
-        String keyHex,
-        String ivHex,
-        String salt,
-        String hmacKeyHex,
-        String hmacHex,
-        Promise promise
+    public static String encryptText(
+            String encodedText,
+            String keyHex,
+            String salt,
+            String ivHex,
+            String hmacKeyHex
     ) {
-        String decryptedText = Crypto.decryptCBC(
-            encryptedText,
-            keyHex,
-            ivHex,
-            salt,
-            hmacKeyHex,
-            hmacHex
+        return Crypto.encryptCBC(
+                encodedText,
+                keyHex,
+                salt,
+                ivHex,
+                hmacKeyHex
         );
-        if (decryptedText == null) {
-            promise.reject("Decryption failed. The archive is possibly tampered with.");
-            return;
-        }
-        promise.resolve(decryptedText);
     }
 
-    @ReactMethod
-    public void generateUUIDs(int count, Promise promise) {
-        String uuids = Crypto.generateUUIDList(10);
-        if (uuids.length() > 0) {
-            promise.resolve(uuids);
-            return;
-        }
-        promise.reject("Generating UUIDs failed.");
+    public static String generateIV() {
+        String ivHex = Crypto.generateRandomBytes();
+        return ivHex.length() > 0 ? ivHex : null;
     }
 
-    @ReactMethod
-    public void generateSaltWithLength(int length, Promise promise) {
+    public static String generateSaltWithLength(int length) {
         String salt = Crypto.generateSalt(length);
-        if (salt.length() > 0) {
-            promise.resolve(salt);
-            return;
-        }
-        promise.reject("Generating Salt failed.");
+        return salt.length() > 0 ? salt : null;
     }
 
-    @ReactMethod
-    public void generateIV(int length, Promise promise) {
-        String ivHex = Crypto.generateRandomBytes(length);
-        if (ivHex.length() > 0) {
-            promise.resolve(ivHex);
-            return;
-        }
-        promise.reject("Generating IV failed.");
+
+    public static String generateUUIDs(int count) {
+        String uuids = count > 0 ? Crypto.generateUUIDList(count) : "";
+        return uuids.length() > 0 ? uuids : null;
     }
 
-    @Override
-    public String getName() {
-        return "Crypto";
+    public static String pbkdf2(String password, String salt, int rounds, int bits) {
+        String derivationHex = Crypto.deriveKeyFromPassword(password, salt, rounds, bits);
+        return derivationHex.length() > 0 ? derivationHex : null;
     }
 }
--- android/app/src/main/java/com/buttercup/CryptoPackage.java
+++ android/app/src/main/java/com/buttercup/CryptoPackage.java
@@ -20,7 +20,7 @@ public class CryptoPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-        modules.add(new Crypto(reactContext));
+        modules.add(new CryptoBridge(reactContext));
         return modules;
     }
 
