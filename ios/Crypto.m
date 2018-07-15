0b65285b2b3efc944b3bd5857b045619166f320b
Perry Mitchell <perry@perrymitchell.net>
2018-07-15 17:17:39 +0300

    Use crypto bridge for testing purposes

---
+++ ios/Crypto.m
@@ -0,0 +1,13 @@
+//
+//  Crypto.m
+//  Buttercup
+//
+//  Created by Perry Mitchell on 15/7/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "Crypto.h"
+
+@implementation Crypto
+
+@end
