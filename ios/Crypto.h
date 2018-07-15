92c7c3c76b6761ce2a6ce6efa520e7774717348e
Perry Mitchell <perry@perrymitchell.net>
2018-07-15 18:19:38 +0300

    Remove comment header

--- ios/Crypto.h
+++ ios/Crypto.h
@@ -1,11 +1,3 @@
-//
-//  Crypto.h
-//  Buttercup
-//
-//  Created by Perry Mitchell on 15/7/18.
-//  Copyright © 2018 Facebook. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface Crypto : NSObject
