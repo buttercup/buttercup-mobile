db6568f3250e80d032edbd137736cc7970b4a52b
ph1p <phil@capshake.com>
2019-08-23 19:12:49 +0200

    Add export to const vars, reorder scripts and add missing line

--- __mocks__/react-native-localize.js
+++ __mocks__/react-native-localize.js
@@ -1,39 +1,24 @@
-const getLocales = () => [
+export const getLocales = () => [
     // you can choose / add the locales you want
     { countryCode: "US", languageTag: "en-US", languageCode: "en", isRTL: false },
     { countryCode: "FR", languageTag: "fr-FR", languageCode: "fr", isRTL: false }
 ];
 
 // use a provided translation, or return undefined to test your fallback
-const findBestAvailableLanguage = () => ({ languageTag: "en-US", isRTL: false });
+export const findBestAvailableLanguage = () => ({ languageTag: "en-US", isRTL: false });
 
-const getNumberFormatSettings = () => ({
+export const getNumberFormatSettings = () => ({
     decimalSeparator: ".",
     groupingSeparator: ","
 });
 
-const getCalendar = () => "gregorian"; // or "japanese", "buddhist"
-const getCountry = () => "US"; // the country code you want
-const getCurrencies = () => ["USD", "EUR"]; // can be empty array
-const getTemperatureUnit = () => "celsius"; // or "fahrenheit"
-const getTimeZone = () => "Europe/Paris"; // the timezone you want
-const uses24HourClock = () => true;
-const usesMetricSystem = () => true;
+export const getCalendar = () => "gregorian"; // or "japanese", "buddhist"
+export const getCountry = () => "US"; // the country code you want
+export const getCurrencies = () => ["USD", "EUR"]; // can be empty array
+export const getTemperatureUnit = () => "celsius"; // or "fahrenheit"
+export const getTimeZone = () => "Europe/Paris"; // the timezone you want
+export const uses24HourClock = () => true;
+export const usesMetricSystem = () => true;
 
-const addEventListener = jest.fn();
-const removeEventListener = jest.fn();
-
-export {
-    findBestAvailableLanguage,
-    getLocales,
-    getNumberFormatSettings,
-    getCalendar,
-    getCountry,
-    getCurrencies,
-    getTemperatureUnit,
-    getTimeZone,
-    uses24HourClock,
-    usesMetricSystem,
-    addEventListener,
-    removeEventListener
-};
+export const addEventListener = jest.fn();
+export const removeEventListener = jest.fn();
