8df0acad5e471d755783d8ddc7aaf7a31b58d600
Perry Mitchell <perry@perrymitchell.net>
2019-08-25 10:19:44 +0300

    Fix google drive token refreshing

--- source/library/googleDrive.js
+++ source/library/googleDrive.js
@@ -33,7 +33,7 @@ export async function authenticate() {
         }
         const { serverAuthCode } = await GoogleSignin.signIn();
         const oauth2Client = new OAuth2Client(googleDriveClientID, null, null);
-        const { tokens } = await oauth2Client.getToken(serverAuthCode);
+        const { tokens } = await oauth2Client.exchangeAuthCodeForToken(serverAuthCode);
         dispatch(setGoogleDriveAuthToken(tokens.access_token));
         dispatch(setGoogleDriveRefreshToken(tokens.refresh_token));
         dispatch(setGoogleDriveAuthenticating(false));
@@ -52,8 +52,10 @@ export async function authenticate() {
 export async function authenticateWithRefreshToken(accessToken, refreshToken) {
     dispatch(setGoogleDriveAuthToken(null));
     dispatch(setGoogleDriveRefreshToken(null));
+    const { googleDriveClientID } = Platform.select(secrets);
     const oauth2Client = new OAuth2Client(googleDriveClientID, null, null);
-    const { tokens } = await oauth2Client.refreshToken(refreshToken);
+    console.log("Client", oauth2Client);
+    const { tokens } = await oauth2Client.refreshAccessToken(refreshToken);
     const newRefreshToken = tokens.refresh_token || refreshToken;
     dispatch(setGoogleDriveAuthToken(tokens.access_token));
     dispatch(setGoogleDriveRefreshToken(newRefreshToken));
