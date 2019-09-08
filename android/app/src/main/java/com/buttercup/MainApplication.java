07da2dd0c10061ee68ebe1cb9e2db9bb5a462661
Sallar Kaboli <sallar@kaboli.org>
2019-09-08 15:30:12 +0300

    Upgrade RN to 59 (breaking)

--- android/app/src/main/java/com/buttercup/MainApplication.java
+++ android/app/src/main/java/com/buttercup/MainApplication.java
@@ -31,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                 new MainReactPackage(),
+            new RNGoogleSigninPackage(),
                 new RNGoogleSigninPackage(),
                 new KeychainPackage(),
                 new RNSecureStoragePackage(),
--- ios/Buttercup/AppDelegate.h
+++ ios/Buttercup/AppDelegate.h
@@ -1,14 +1,15 @@
 /**
- * Copyright (c) 2017-present, Buttercup, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the GNU GPLv3 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
+#import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
-@property (nonatomic, strong) UIWindow *window;
+@property(nonatomic, strong) UIWindow *window;
 
 @end
