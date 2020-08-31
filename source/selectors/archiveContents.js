6449a52d9da6cb4377d050fbc4f3e082948ad43c
Florian Wallner <florian.wallner@exxcellent.de>
2020-08-31 21:14:40 +0200

    Remove unused function

--- source/selectors/archiveContents.js
+++ source/selectors/archiveContents.js
@@ -40,24 +40,6 @@ export function getGroups(state) {
     return state[STATE_KEY].groups;
 }
 
-export function getGroupsUnderID(state, id) {
-    const groups = getGroups(state);
-    const findGroup = groups => {
-        let foundGroup = groups.find(group => group.id === id) || null;
-        if (!foundGroup) {
-            for (let i = 0, groupsLen = groups.length; i < groupsLen; i += 1) {
-                foundGroup = findGroup(groups[i].groups || []) || null;
-                if (foundGroup !== null) {
-                    break;
-                }
-            }
-        }
-        return foundGroup;
-    };
-    const foundGroup = id.toString() === "0" ? { groups } : findGroup(groups);
-    return foundGroup && foundGroup.groups ? sortGroups(foundGroup.groups) : [];
-}
-
 export function getSelectedArchive(state) {
     const id = getSelectedSourceID(state);
     const archiveManager = getSharedArchiveManager();
