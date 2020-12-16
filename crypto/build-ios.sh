1a893a31c6469e3139fe4bbd79fae830358155b9
Sallar Kaboli <sallar@kaboli.org>
2020-12-16 16:13:21 +0200

    Fix build issues with iOS

--- crypto/build-ios.sh
+++ crypto/build-ios.sh
@@ -4,8 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 rustup target add aarch64-apple-ios
-rustup target add armv7-apple-ios
-rustup target add i386-apple-ios
 rustup target add x86_64-apple-ios
 
 cargo lipo --release
