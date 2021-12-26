46d792de3c97faef99ce56681e4e7a4f0a224a38
Perry Mitchell <perry@perrymitchell.net>
2021-12-26 16:34:33 +0200

    Load gesture handler earlier - fixes #280

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import "./shim";
 
 import { AppRegistry } from "react-native";
 import { App } from "./source/App";
+import "react-native-gesture-handler";
 import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
