b47472fd517e927dadef6e61f5284149a520750c
se1exin <selexin@selexin.com>
2022-01-02 12:38:50 +1100

    Update for Android Autofill bridge

--- source/App.tsx
+++ source/App.tsx
@@ -12,7 +12,7 @@ interface AppProps {
 }
 
 function _App(props: AppProps = {}) {
-    const isAutofill: boolean = useMemo(() => props.isContextAutoFill === 1, [props]);
+    const isAutofill: boolean = useMemo(() => [1, true].indexOf(props.isContextAutoFill) > -1, [props]);
     const handleCoverScreen = useCallback((appState: AppStateStatus) => {
         if (isAutofill) return;
         if (appState === "inactive" || appState === "background") {
@@ -20,6 +20,7 @@ function _App(props: AppProps = {}) {
         }
     }, [isAutofill]);
     useAppStateDebouncedCallback(handleCoverScreen);
+    console.log('isAutofill', isAutofill, props);
     if (isAutofill) {
         return (
             <AutofillApp
