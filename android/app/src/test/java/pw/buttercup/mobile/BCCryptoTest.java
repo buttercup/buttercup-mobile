fed2eb6b6cd24a9c74cee78313d012f56d7fe373
Perry Mitchell <perry@perrymitchell.net>
2022-01-05 21:48:58 +0200

    Test decryption

--- android/app/src/test/java/pw/buttercup/mobile/BCCryptoTest.java
+++ android/app/src/test/java/pw/buttercup/mobile/BCCryptoTest.java
@@ -1,14 +1,27 @@
 package pw.buttercup.mobile;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BCCryptoTest {
 
-//    @Test
-//    void decryptText() {
-//    }
+    @Test
+    void decryptText_decryptsEncryptedText() throws Exception {
+        String derivedKey = BCDerivation.deriveKeyFromPassword("test", "abc123", 10000, 512);
+        String keyHex = derivedKey.substring(0, 64);
+        String hmacKeyHex = derivedKey.substring(64);
+        String saltHex = BCCrypto.generateSalt(16);
+        String ivHex = BCCrypto.generateIV();
+        // Encrypt
+        String source = BCHelpers.stringToBase64("This is some text!");
+        String encrypted = BCCrypto.encryptText(source, keyHex, saltHex, ivHex, hmacKeyHex);
+        // Extract
+        String[] parts = encrypted.split("\\|");
+        // Decrypt
+        String decrypted = BCCrypto.decryptText(parts[0], keyHex, parts[2], parts[3], hmacKeyHex, parts[1]);
+        String decoded = BCHelpers.base64ToString(decrypted);
+        assertEquals("This is some text!", decoded);
+    }
 
     @Test
     void encryptText_encryptsText() throws Exception {
